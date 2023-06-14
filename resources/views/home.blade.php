f53fca903e1e23f74b7ed880c8e8416f41f61bc7
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-06-14 18:58:05 +0300

    posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,53 +31,47 @@
                 </div>
                 <div class="card-content">
                     <div class="card-body login">
-                     
-                        <form class="form-horizontal form-simple" method="POST" action="">
-                            <input type="hidden" name="_token" >                          
+                    @auth
+                        <form class="form-horizontal form-simple" action="{{ route('posts') }}" method="post" >
+                        @csrf    
+                        <input type="hidden" name="_token" >                          
                               <fieldset class="form-group position-relative has-icon-left">
-                                <input type="text"
-                                    class="form-control form-control-lg input-lg "
-
-                                    placeholder="Title" required>
-                                <div class="form-control-position">
-                                   
-                                </div>
+                              <input type="text" name="title" class="form-control form-control-lg input-lg rounded-lg @error('body') border-red-500 @enderror" placeholder="Title" required>
                               </fieldset>
                               <fieldset class="form-group position-relative has-icon-left">
-                                <input type="text"
+                                <input type="text" name="platform"
                                     class="form-control form-control-lg input-lg "
                                     placeholder="Platform (zoom, Google meet, etc)">
-                              
+                                   
                               </fieldset>
                               <fieldset class="form-group position-relative has-icon-left">
-                                <input type="text"
+                                <input type="text" name="link"
                                     class="form-control form-control-lg input-lg "
                                     placeholder="Link">
-                              
+                                  
                               </fieldset>
                               
                               <fieldset class="form-group position-relative has-icon-left">
-                                <input type="date"
+                                <input type="date"  name="date"
                                       class="form-control form-control-lg input-lg"
                                       required
                                       placeholder="Select a date">
-                                <div class="form-control-position">
-                                </div>
+                                   
                               </fieldset>
 
                               <fieldset class="form-group position-relative has-icon-left">
-                                <input type="time"
+                                <input type="time"  name="time"
                                       class="form-control form-control-lg input-lg"
                                       required
                                       placeholder="Select a time">
-                                <div class="form-control-position">
-                                </div>
+                                     
                               </fieldset>
 
-                            <button type="submit" @click.prevent="addreminder()" class="btn blue-bg btn-lg btn-block">
+                            <button type="submit" class="btn blue-bg btn-lg btn-block">
                               Add
                             </button>
                         </form>
+                    @endauth
                     </div>
                 </div>
             </div>
