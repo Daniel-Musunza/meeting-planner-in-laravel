5d929de9cefb33ad75a8585216b92fb793da4be5
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-06-21 17:19:28 +0300

    mailer

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
         if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
             return back()->with('status', 'Invalid login details');
         }
-
         return redirect()->route('posts');
     }
 }
